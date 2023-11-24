aa4174ca1f080d75c3e6940f4b6e89fca70e8662
Yuuki Wesp <ls-micro@ya.ru>
2023-11-24 18:25:50 +0300

    add wallpaper command

---
+++ src/commands/images/WallPaperGenCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace nai.commands.images;
+
+public class WallPaperGenCommand : ImageGenCommand
+{
+    public override List<string> Aliases => new()
+    {
+        "/wallpaper"
+    };
+    protected override bool IsSfw() => false;
+
+    protected override ValueTask OnFillAdditionalData(NovelAIinput input)
+    {
+        input.parameters.sm = true;
+        return base.OnFillAdditionalData(input);
+    }
+
+    protected override (int x, int y) GetSize() => new(1920, 1088);
+}
