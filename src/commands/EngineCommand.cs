2a105280b1c1289018497f745c9487beed56fdd9
Yuuki Wesp <ls-micro@ya.ru>
2023-11-25 03:35:37 +0300

    add engine command

---
+++ src/commands/EngineCommand.cs
@@ -0,0 +1,44 @@
+﻿using nai.i18n;
+using nai.nai;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace nai.commands;
+
+public class EngineCommand : Command
+{
+    public override List<string> Aliases
+        => new() { "/engine" };
+    public override async ValueTask ExecuteAsync(string cmdText, CancellationToken ct)
+    {
+        if (string.IsNullOrEmpty(cmdText))
+        {
+            await BotClient.SendTextMessageAsync(
+                chatId: CharId,
+                replyToMessageId: Message.MessageId,
+                text: string.Format(Locale.Get(Locales.ActiveEngine), User.GetSelectedEngine(Config.GetNaiSettings()).key, string.Join(", ", NovelAIEngine.All.Select(x => $"`{x.key}`"))),
+                cancellationToken: ct, parseMode: ParseMode.Markdown).ConfigureAwait(false);
+            return;
+        }
+
+        if (!NovelAIEngine.HasKey(cmdText))
+        {
+            await BotClient.SendTextMessageAsync(
+                chatId: CharId,
+                replyToMessageId: Message.MessageId,
+                text: string.Format(Locale.Get(Locales.EngineNotFound), cmdText.Trim()),
+                cancellationToken: ct).ConfigureAwait(false);
+            return;
+
+        }
+
+        var engine = NovelAIEngine.ByKey(cmdText);
+        await User.SetActiveEngine(engine);
+
+        await BotClient.SendTextMessageAsync(
+            chatId: CharId,
+            replyToMessageId: Message.MessageId,
+            text: string.Format(Locale.Get(Locales.EngineHasSwitched), engine.key),
+            cancellationToken: ct).ConfigureAwait(false);
+    }
+}
